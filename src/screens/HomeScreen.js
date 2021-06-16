c4d1fc5b15774facf5998f18e0cd7ee9b2f847fa
Santu <santumete01@gmail.com>
2021-06-16 20:30:42 +0530

    load product from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import data from '../components/data';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+// import data from '../components/data';
 import Product from '../components/Product';
+ 
 
 const HomeScreen = () => {
+    const [products,setProducts] = useState([]); 
+    useEffect(() => {
+        const fetchData= async()=>{
+            const {data} = await axios.get('/api/products');
+            setProducts(data);
+        };
+        fetchData();
+    }, []);
     return (
         <div>
             <div className= "row center">
